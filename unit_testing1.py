c5555d947586c80dcf7f84b341efe2f401d10ac1
FreyGeospatial <jordan@freygeospatial.com>
2022-03-26 18:32:47 -0400

    upload files from tutorial

---
+++ unit_testing1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 19:36:53 2022
+
+@author: jfrey
+"""
+
+# example of a test. If true, there is no output
+assert sum([1, 2, 3]) == 6, "Should be 6"
+assert sum([1, 2, 3]) == 7, "Should be 6"
+
