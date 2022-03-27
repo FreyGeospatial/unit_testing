cf6c8a881887021cf8401db1593b2f97c523fc23
FreyGeospatial <jordan@freygeospatial.com>
2022-03-27 14:08:54 -0400

    changing repo structure. Parent folders will represent different unit testing tutorials

---
+++ realpython/test_sum_unittest.py
@@ -0,0 +1,19 @@
+# unittest contains both a testing framework and a test runner.
+import unittest
+
+# unittest requires that:
+
+# --- You put your tests into classes as methods
+# --- You use a series of special assertion methods in the unittest.TestCase class 
+#     instead of the built-in assert statement
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
